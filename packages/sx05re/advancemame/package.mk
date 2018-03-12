26d3a51a0b2999d9f872884883284c34f088b760
chataigne73 <chataigne73@gmail.com>
2018-03-12 17:35:51 +0100

    More clean up for advmame

--- packages/sx05re/advancemame/package.mk
+++ packages/sx05re/advancemame/package.mk
@@ -4,14 +4,14 @@
 ################################################################################
 
 PKG_NAME="advancemame"
-PKG_VERSION="dcaec4b"
+PKG_VERSION="7141805"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
 PKG_SITE="https://github.com/amadvance/advancemame"
 PKG_URL="https://github.com/amadvance/advancemame/archive/$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="advancemame-$PKG_VERSION*"
-PKG_DEPENDS_TARGET="toolchain freetype slang alsa SDL2"
+PKG_DEPENDS_TARGET="toolchain freetype slang alsa"
 PKG_SECTION="sx05re/mod"
 PKG_SHORTDESC="A MAME and MESS port with an advanced video support for Arcade Monitors, TVs, and PC Monitors "
 PKG_LONGDESC="A MAME and MESS port with an advanced video support for Arcade Monitors, TVs, and PC Monitors "
@@ -20,18 +20,27 @@ PKG_AUTORECONF="no"
 
 
 make_target() {
-#CFLAGS="$CFLAGS -mfpu=neon-fp-armv8"
-#CFLAGS="$CFLAGS -mfpu=neon-vfpv4 -march=armv7-a"
 ./autogen.sh
-./configure --host=arm --enable-fb --enable-sdl2 --enable-freetype --prefix=$INSTALL/usr --with-freetype-prefix=$SYSROOT_PREFIX/usr/ --with-sdl2-prefix=$SYSROOT_PREFIX/usr/ --enable-slang
-
-#./configure --host=arm --enable-alsa --enable-fb --enable-freetype --prefix=$INSTALL/usr --with-freetype-prefix=$SYSROOT_PREFIX/usr/ --enable-slang
-
- 
+./configure --prefix=/usr --datadir=/usr/share/ --datarootdir=/usr/share/ --host=arm --enable-fb --enable-freetype --with-freetype-prefix=$SYSROOT_PREFIX/usr/ --enable-slang
 make 
+}
+
+makeinstall_target() {
+ : not
+}
+
+post_make_target() { 
+# Cleanup unnecessary files
 mkdir -p $INSTALL/usr/share/advance
    cp -r $PKG_DIR/config/* $INSTALL/usr/share/advance/
 mkdir -p $INSTALL/usr/bin
    cp -r $PKG_DIR/bin/* $INSTALL/usr/bin
 chmod +x $INSTALL/usr/bin/advmame.sh
+
+cp -r $PKG_BUILD/obj/mame/generic/blend/advmame $INSTALL/usr/bin
+cp -r $PKG_BUILD/support/category.ini $INSTALL/usr/share/advance
+cp -r $PKG_BUILD/support/sysinfo.dat $INSTALL/usr/share/advance
+cp -r $PKG_BUILD/support/history.dat $INSTALL/usr/share/advance
+cp -r $PKG_BUILD/support/hiscore.dat $INSTALL/usr/share/advance
+cp -r $PKG_BUILD/support/event.dat $INSTALL/usr/share/advance
 }
